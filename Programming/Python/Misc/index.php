ba47f6f97d814a95097958c15eeefbefbfef19e1
Wadaane <salimrichab@gmail.com>
2017-11-26 03:32:58 +0200

    Deleted files

--- Programming/Python/Misc/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	echo "<html><head><title>WadaanePi</title></head></html>";
-	echo date('d-m-Y H:i');
-?>
--- Programming/Python/Misc/matplotlib_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-print('start')
-
-x = np.arange(0, 7, 0.1);
-y = np.sin(x)
-plt.plot(x, y)
-plt.show()
-print('end')
