a495040a3459dfd0d3589c10bfe8b33fd53adca6
Wadaane <salimrichab@gmail.com>
2017-11-28 15:56:26 +0200

    reformatted renameFiles.py

--- Python/renameFiles.py
+++ Python/renameFiles.py
@@ -1,67 +1,69 @@
 import os
 import re
 
-        ## This program Rename multiple files in a directory located
-        ## in "path" and has common "pattern" to "newTitle+newPattern+part of old file to be kept"
-        ## File name text to be kept is "offset" number of character to the right of pattern
-        ## Exemple: "Game of throne S01E01.avi", "GOT_S01E02.avi", "Gameofthrones_S01E01.mkv"
-        ## renamed to "GOT S01E01.avi", "GOT S01E02.avi", "GOT S01E03.mkv"
-        ## pattern is "S01E" and offset is '2' and "newTitle" is "GOT "
 
-def renameFiles():
-    #path, newTitle, pattern, newPattern, offset
+# This program Rename multiple files in a directory located
+# in "path" and has common "pattern" to "newTitle+newPattern+part of old file to be kept"
+# File name text to be kept is "offset" number of character to the right of pattern
+# Exemple: "Game of throne S01E01.avi", "GOT_S01E02.avi", "Gameofthrones_S01E01.mkv"
+# renamed to "GOT S01E01.avi", "GOT S01E02.avi", "GOT S01E03.mkv"
+# pattern is "S01E" and offset is '2' and "newTitle" is "GOT "
+
+def rename_files():
+    # path, new_title, pattern, new_pattern, offset
     repeat = True
     while repeat:
         path = input('Path = ')
-        newTitle = input('Title = ')
+        new_title = input('Title = ')
         pattern = input('pattern = ')
-        newPattern = input('new Pattern = ')
+        new_pattern = input('new Pattern = ')
         offset = int(input('Max Offset = '))
-        
+
         # List all files in the directory
         files = os.listdir(path)
         i = 0
-        newNames = []
-        
+        new_names = []
+
         # Iterate through files
         for file in files:
             # Split file name from files extensions
-            fileName, fileExt = os.path.splitext(file)
+            file_name, file_ext = os.path.splitext(file)
 
             # Detect where the pattern start is located
             # Returns -1 if its not found
-            index =fileName.find(pattern)            
+            index = file_name.find(pattern)
 
             if index != -1:
-                if newPattern == '':
+                if new_pattern == '':
                     # Crop the file name from index to offset
-                    fileName = fileName[index:index+int(len(pattern))+offset]
+                    file_name = file_name[index:index + int(len(pattern)) + offset]
                 else:
                     # Crop the file number
-                    fileName = fileName[index+int(len(pattern)):index+int(len(pattern))+offset]
-                    fileNumber = re.findall(r'\b\d+\b', fileName)
-                    fileName = newPattern + str(fileNumber[0]).zfill(2)
+                    file_name = file_name[index + int(len(pattern)):index + int(len(pattern)) + offset]
+                    file_number = re.findall(r'\b\d+\b', file_name)
+                    file_name = new_pattern + str(file_number[0]).zfill(2)
                 # Assign new Common tiltle head and reassign extension                
-                newName = newTitle +fileName+ fileExt
-                print("     "+file + "      "+ newName)
-                newNames.insert(files.index(file),newName)
-            i= i+1
-        
+                new_name = new_title + file_name + file_ext
+                print("     " + file + "      " + new_name)
+                new_names.insert(files.index(file), new_name)
+            i = i + 1
+
         doit = input("Continue ? (y/n) = ") == 'y'
-        
+
         if doit:
-            i=0
+            i = 0
             for f in files:
-                if f.find(pattern)!= -1:
+                if f.find(pattern) != -1:
                     # Rename the file
-                    os.rename(path+'\\' + f, path+'\\' + newNames[i])
-                    i = i+1
+                    os.rename(path + '\\' + f, path + '\\' + new_names[i])
+                    i = i + 1
         # Exit or Restart
-        
+
         repeat = input("Exit ? (y/n) = ") == 'n'
         os.system("cls")
     exit()
 
+
 # Means run my function as this file main function
 if __name__ == "__main__":
-    renameFiles()
+    rename_files()
