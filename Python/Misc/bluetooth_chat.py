e2d9594a59558a147e5c3bbdb1881dccefd80163
Wadaane <salimrichab@gmail.com>
2017-12-22 17:16:24 +0200

    Added bluetooth_chat.py

---
+++ Python/Misc/bluetooth_chat.py
@@ -0,0 +1,29 @@
+import bluetooth
+from pykeyboard import PyKeyboard
+
+k = PyKeyboard()
+server_socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+
+port = 1
+
+server_socket.bind(("", port))
+server_socket.listen(1)
+
+client_socket, address = server_socket.accept()
+print('Accepted connection from', address)
+
+while True:
+    try:
+        data = client_socket.recv(1024)
+        s = data.decode()
+        if len(s)>0:    
+#            print('Received: ' + s)
+            k.type_string(s)
+            print()
+        client_socket.send("Hello From Pi !!!")
+        client_socket.send('\n')
+    except:
+        print('Connexion closed')
+        break
+client_socket.close()
+server_socket.close()
